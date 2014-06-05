08fb59fdc21fd0d724782b1a1d65c67ef5918560
Fabian Bull <bull.fabian@googlemail.com>
2014-06-05 10:14:07 +0200

    Added stubs for chi square tests.

--- src/main/scala/org/lanyard/test/ChiSquareTest.scala
+++ src/main/scala/org/lanyard/test/ChiSquareTest.scala
@@ -1,4 +1,12 @@
 package org.lanyard.test
 
 /** The Chi-Square-Test */
-case class ChiSquareTest()
+object ChiSquareTest {
+
+  def one( sample: Array[Double], expected: Array[Double], constraints: Int = 1 ): ChiSquareTestResult = ???
+
+  def two( sample1: Array[Double], sample2: Array[Double], constraints: Int = 1 ): ChiSquareTestResult = ???
+
+  case class ChiSquareTestResult( pValue: Double, df: Int, statistic: Double )
+
+}
